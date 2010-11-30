98135e67b32025124c9181a3183b7ad67ba1cb1a
Maurizio Leo <maurizioleo@gmail.com>
2010-11-30 08:09:27 -0700

    Leo - added iperf client startup for Roma.

--- run_bt.py
+++ run_bt.py
@@ -33,7 +33,7 @@ def ssh_command (user, host, password):
 	print 'Login to %s successful.' % host
 	return child
 
-def start_iperf_server (child, srcMachine, dstMachine=None, server=True):
+def start_iperf (child, srcMachine, dstMachine=None, server=True):
 	"""
 	"""
 	if(server == True):
@@ -41,36 +41,35 @@ def start_iperf_server (child, srcMachine, dstMachine=None, server=True):
 		child.sendline('/bin/bash -c "iperf -s > iperf_log.txt"')
 	else:
 		print 'Starting iperf client on: %s, to %s.' % (srcMachine, dstMachine)
-		child.sendline('/bin/bash -c "iperf -c %s -i 5 > iperf_log.txt"' %dstMachine) #-t 120
+		child.sendline('/bin/bash -c "iperf -c %s -i 5 > iperf_log.txt"' % dstMachine) #-t 120
 
 def main ():
 	GWAGDY = '192.168.1.110'
 	GHELMY = '192.168.110.2'
 	MICKEY = '192.168.110.4'
 	SPOOF = '192.168.110.3'
-	
+
 	BARCA = '192.168.1.121'
 	INTER = '192.168.121.2'
 	GENOA = '192.168.121.3'
-	ROMA = '192.168.110.4'
+	ROMA = '192.168.121.4'
 
 	user = 'maurizio'
 	password = getpass.getpass('Password: ')
 
 	# Start GWAGDY's iperf server
-	print 'Setup Gwagdy.'
+	print 'Setup Gwagdy with iperf server.'
 	gwagdy = ssh_command (user, GWAGDY, password)
-	start_iperf_server(gwagdy, GWAGDY)
-	#time.sleep(10);
+	start_iperf (gwagdy, GWAGDY)
 	
 	# Start BARCA's iperf server
-	print '\nSetup Barca.'
+	print '\nSetup Barca with iperf server.'
 	barca = ssh_command (user, BARCA, password)
-	start_iperf_server(barca, BARCA)
-	#time.sleep(10);
-
+	start_iperf (barca, BARCA)
 
 	# Start GHELMY's OpenTracker
+	#print '\nSetup GHELMY with OpenTracker.'
+	
 	
 	# Start GHELMY's bittornado dl/seed
 	
@@ -81,9 +80,12 @@ def main ():
 	# Start GENOA's bittornado dl/seed
 	
 	# Start MICKEY'S iperf client
+	#print 'Setup MICKEY with iperf client.'
 	
 	# Start ROMA's iperf client
-	
+	print '\nSetup Roma with iperf client.'	
+	roma = ssh_command(user, ROMA, password)
+	start_iperf (roma, ROMA, GWAGDY, False)
 
 	raw_input("\n\nExperiment running.  Hit <Enter> to quit.\n")
 	
